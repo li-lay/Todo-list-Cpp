4c38b42ccf65c4637025c155837cf6bbba1604f2
LILAY <contact@lilay.dev>
2024-09-29 14:29:33 +0700

    format code

--- app.cpp
+++ app.cpp
@@ -1,8 +1,8 @@
 // To-do list in C++ with class
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <cstdlib>
 using namespace std;
 
 // Check platforms
@@ -13,164 +13,155 @@ using namespace std;
 #endif
 
 class Todo {
-  private:
-    struct tasks
-    {
-      int id;
-      string task;
-    };
-    int ID = 0; // IDs
-    bool completed; // completion
+private:
+  struct tasks {
+    int id;
+    string task;
+  };
+  int ID = 0;     // IDs
+  bool completed; // completion
 
-  public:
+public:
+  void clearScreen() { system(CLEAR_SCREEN); }
 
-    void clearScreen() {
-      system(CLEAR_SCREEN);
-    }
+  void logo() {
+    cout << "#==============================#\n";
+    cout << "|          Todo lists          |\n";
+    cout << "#==============================#\n";
+  }
 
-    void logo() {
-      cout << "#==============================#\n";
-      cout << "|          Todo lists          |\n";
-      cout << "#==============================#\n";
-    }
+  void mainMenu() {
+    cout << "\n#==============================#\n";
+    cout << "|                              |\n";
+    cout << "|      [1] Add a new task      |\n";
+    cout << "|      [2] Display tasks       |\n";
+    cout << "|      [3] Search task         |\n";
+    cout << "|      [4] Delete a task       |\n";
+    cout << "|      [5] About us            |\n";
+    cout << "|      [0] Exit                |\n";
+    cout << "|                              |\n";
+    cout << "#==============================#\n";
+  }
 
-    void mainMenu() {
-      cout << "\n#==============================#\n";
-      cout << "|                              |\n";
-      cout << "|      [1] Add a new task      |\n";
-      cout << "|      [2] Display tasks       |\n";
-      cout << "|      [3] Search task         |\n";
-      cout << "|      [4] Delete a task       |\n";
-      cout << "|      [5] About us            |\n";
-      cout << "|      [0] Exit                |\n";
-      cout << "|                              |\n";
-      cout << "#==============================#\n";
-    }
+  void about() {
+    clearScreen();
+    cout << "#==============================#\n";
+    cout << "|                              |\n";
+    cout << "|            Team 6            |\n";
+    cout << "|                              |\n";
+    cout << "|        1. Hun LimHeng        |\n";
+    cout << "|        2.                    |\n";
+    cout << "|        3. Chhou Lilay        |\n";
+    cout << "|                              |\n";
+    cout << "#==============================#\n";
+  }
 
-    void about() {
-      clearScreen();
-      cout << "#==============================#\n";
-      cout << "|                              |\n";
-      cout << "|            Team 6            |\n";
-      cout << "|                              |\n";
-      cout << "|        1. Hun LimHeng        |\n";
-      cout << "|        2.                    |\n";
-      cout << "|        3. Chhou Lilay        |\n";
-      cout << "|                              |\n";
-      cout << "#==============================#\n";
-    }
+  void setId(int id) { ID = id; }
 
-    void setId(int id) {
-      ID = id;
-    }
+  void print(tasks s) { cout << " [" << s.id << "] " << s.task << endl; }
 
-    void print(tasks s) {
-      cout << " [" << s.id << "] " << s.task << endl;
-    }
+  void addTask() {
+    clearScreen();
+    logo();
+    tasks new_task;
+    cout << "Enter new task: ";
+    cin.get();
+    getline(cin, new_task.task);
+    ID++;
 
-    void addTask() {
-      clearScreen();
-      logo();
-      tasks new_task;
-      cout << "Enter new task: ";
-      cin.get();
-      getline(cin, new_task.task);
-      ID++;
+    ofstream write;
+    write.open("todo.txt", ios::app);
+    write << "\n" << ID;
+    write << "\n" << new_task.task;
+    write.close();
 
-      ofstream write;
-      write.open("todo.txt", ios::app);
-      write << "\n" << ID;
-      write << "\n" << new_task.task ;
-      write.close();
-
-      write.open("id.txt");
-      write << ID;
-      write.close();
+    write.open("id.txt");
+    write << ID;
+    write.close();
 
     char ch;
-    cout<<"\nDo you want to add more task? y/n: ";
-    cin>> ch;
+    cout << "\nDo you want to add more task? y/n: ";
+    cin >> ch;
 
-    if(ch == 'y'){
-        addTask();
-    }
-    else{
+    if (ch == 'y') {
+      addTask();
+    } else {
       clearScreen();
       cout << "\n!!!Task has been added successfully!!!\n\n";
       return;
     }
   }
-    void readTask() {
-      clearScreen();
-      logo();
-      tasks read_task;
+  void readTask() {
+    clearScreen();
+    logo();
+    tasks read_task;
+    ifstream read;
+    read.open("todo.txt");
+    cout << " Your current tasks:\n\n";
+
+    while (!read.eof()) {
+      read >> read_task.id;
+      read.ignore();
+      getline(read, read_task.task);
+      print(read_task);
+    }
+    read.close();
+  }
+
+  int searchTask() {
+    clearScreen();
+    logo();
+    int id;
+    cout << "Enter task id: ";
+    cin >> id;
+    tasks search_task;
+    ifstream read;
+    read.open("todo.txt");
+
+    while (!read.eof()) {
+      read >> search_task.id;
+      read.ignore();
+      getline(read, search_task.task);
+      if (search_task.id == id) {
+        print(search_task);
+        return id;
+      }
+    }
+    return 0;
+  }
+
+  void deleteTask() {
+    clearScreen();
+    logo();
+    int id = searchTask();
+    char opt;
+    cout << "Delete this task? (y/n): ";
+    cin >> opt;
+    if (opt == 'y') {
+      tasks delete_task;
+      ofstream tempFile;
+      tempFile.open("temp.txt");
       ifstream read;
       read.open("todo.txt");
-      cout << " Your current tasks:\n\n";
 
       while (!read.eof()) {
-        read >> read_task.id;
+        read >> delete_task.id;
         read.ignore();
-        getline(read, read_task.task);
-        print(read_task);
+        getline(read, delete_task.task);
+        if (delete_task.id != id) {
+          tempFile << "\n" << delete_task.id;
+          tempFile << "\n" << delete_task.task;
+        }
       }
       read.close();
-      }
-
-    int searchTask() {
-      clearScreen();
-      logo();
-      int id;
-      cout << "Enter task id: ";
-      cin >> id;
-      tasks search_task;
-      ifstream read;
-      read.open("todo.txt");
-
-      while (!read.eof()) {
-        read >> search_task.id;
-        read.ignore();
-        getline(read, search_task.task);
-        if (search_task.id == id) {
-            print(search_task);
-            return id;
-        }
+      tempFile.close();
+      remove("todo.txt");
+      rename("temp.txt", "todo.txt");
+      cout << "\nTask deleted successfully\n\n";
+    } else {
+      cout << "\nDeletion cancelled!\n";
     }
-    }
-
-    void deleteTask() {
-      clearScreen();
-      logo();
-      int id = searchTask();
-      char opt;
-      cout << "Delete this task? (y/n): ";
-      cin >> opt;
-      if (opt == 'y') {
-        tasks delete_task;
-        ofstream tempFile;
-        tempFile.open("temp.txt");
-        ifstream read;
-        read.open("todo.txt");
-
-        while (!read.eof()) {
-          read >> delete_task.id;
-          read.ignore();
-          getline(read, delete_task.task);
-          if (delete_task.id != id) {
-            tempFile << "\n" << delete_task.id;
-            tempFile << "\n" << delete_task.task;
-          }
-        }
-        read.close();
-        tempFile.close();
-        remove("todo.txt");
-        rename("temp.txt", "todo.txt");
-        cout << "\nTask deleted successfully\n\n";
-      } 
-      else {
-        cout << "\nDeletion cancelled!\n";
-      }
-    }
+  }
 };
 
 int main() {
@@ -182,8 +173,7 @@ int main() {
     int i;
     read >> i;
     app.setId(i);
-  }
-  else {
+  } else {
     app.setId(0);
   }
   read.close();
@@ -191,15 +181,13 @@ int main() {
   int option = 1;
   app.logo();
 
-  do
-  {
+  do {
     app.mainMenu();
 
     cout << "Enter option: ";
     cin >> option;
 
-    switch (option)
-    {
+    switch (option) {
     case 0:
       app.clearScreen();
       return 0;
